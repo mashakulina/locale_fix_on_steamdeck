436639f0c60487fdb4b1fdcc5e957232f4f3ba9a
Кулина Мария <103326791+mashakulina@users.noreply.github.com>
2024-05-04 00:52:04 +0300

    Update environment_update.sh

--- environment_update.sh
+++ environment_update.sh
@@ -15,11 +15,6 @@ else
   sleep 1
   sed -i '5 a\$ locale\nLANG=ru_RU.UTF-8\nLANGUAGE=ru_RU:ru:en_GB:en\nLC_CTYPE="ru_RU.UTF-8"\nLC_NUMERIC="ru_RU.UTF-8"\nLC_TIME="ru_RU.UTF-8"\nLC_COLLATE="ru_RU.UTF-8"\nLC_MONETARY="ru_RU.UTF-8"\nLC_MESSAGES="ru_RU.UTF-8"\nLC_PAPER="ru_RU.UTF-8"\nLC_NAME="ru_RU.UTF-8"\nLC_ADDRESS="ru_RU.UTF-8"\nLC_TELEPHONE="ru_RU.UTF-8"\nLC_MEASUREMENT="ru_RU.UTF-8"\nLC_IDENTIFICATION="ru_RU.UTF-8"\nLC_ALL=\n' /etc/environment
   
-  # Update the environment configuration file
-  #echo "Update environment..."
-  #sleep 1
-  #update-environment
-  
   # Reboot after a countdown
   for i in {3..1}; do
     echo "Reboot in $i seconds..."
